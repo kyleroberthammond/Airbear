d9b93af15e6369757efbbfb7d76d131ce47a0c15
Kyle Hammond - MJH Engineering Ltd <kyle.hammond@mjh.co.nz>
2024-10-31 23:36:41 +1300

    Modal is opening and inital structure is complete, ironing out the new project issues

--- frontend-dev/src/index.jsx
+++ frontend-dev/src/index.jsx
@@ -1,15 +1,25 @@
 import { render } from "preact";
 
-import "../css/style.css";
+// import "../css/style.css";
 import { useState } from "preact/hooks";
-
-import { Grid } from "react-flexbox-grid";
+import { Button, Col, Container, Modal, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import map from "lodash.map";
+import clone from "lodash.clone";
 
 export function App() {
-  const [layoutObject, setLayoutObject] = useState({}); // The layout config json, can import a default one.
-  const [data, setData] = useState({});
-  const [number, setNumber] = useState(0); // Initial number
+  const [layoutObject, setLayoutObject] = useState({ rows: [] }); // The layout config json, can import a default one.
+  const [gaugeModalOpen, setGaugeModalOpen] = useState(false);
+  console.log(gaugeModalOpen);
 
+  // Rows > Guages
+  // Example object
+  // {rows:[
+  //   {guages:[]},
+  //   {guages:[]},
+  // ]}
+
+  // const [data, setData] = useState({});
   // useEffect(() => {
   //   // This is the onload. You would setup the serial events incoming here.
   //   // As one hits you would update the data by calling setData which is just an object.
@@ -18,10 +28,58 @@ export function App() {
   //   // Waiting on my esp32 to arrive to test all this.
   // }, [])
 
+  const { rows } = layoutObject;
+
+  const addRow = () => {
+    let newLayoutObject = clone(layoutObject);
+    newLayoutObject.rows.push({
+      gauges: [],
+    }); // Setup the new row object
+
+    // Update the layout object with the new rows.
+    setLayoutObject(newLayoutObject);
+  };
+
   return (
-    <div>
-    </div>
+    <Container fluid>
+      {map(rows, (row, index) => {
+        return (
+          <RowOfGauges
+            row={row}
+            key={index}
+            index={index}
+            toggleGaugeModal={() => {
+              setGaugeModalOpen(!gaugeModalOpen);
+            }}
+          />
+        );
+      })}
+
+      <Row className="mt-2">
+        <Col>
+          <Button onClick={addRow}>Add Row</Button>
+        </Col>
+      </Row>
+
+      <GaugeModal isOpen={gaugeModalOpen} />
+    </Container>
   );
 }
+
+const RowOfGauges = (props) => {
+  const { row, index, toggleGaugeModal } = props;
+  return (
+    <Row className="mt-2">
+      <Col>
+        <Button onClick={toggleGaugeModal}>Add Gauge</Button>
+      </Col>
+    </Row>
+  );
+};
+
+const GaugeModal = (props) => {
+  const { isOpen } = props;
+  return <Modal isOpen={isOpen} trigger="legacy">Add new gauge here</Modal>;
+};
 
 render(<App />, document.getElementById("app"));
