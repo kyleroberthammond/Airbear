225c318bfbbc0dcd0bb874b575fc263c286f98d7
Kyle Hammond - MJH Engineering Ltd <kyle.hammond@mjh.co.nz>
2024-10-31 21:38:27 +1300

    Setup vite with preact

---
+++ frontend-dev/src/index.jsx
@@ -0,0 +1,22 @@
+import { render } from 'preact';
+
+import './style.css';
+
+export function App() {
+	return (
+		<div>
+			Just a simple preact app
+		</div>
+	);
+}
+
+function Resource(props) {
+	return (
+		<a href={props.href} target="_blank" class="resource">
+			<h2>{props.title}</h2>
+			<p>{props.description}</p>
+		</a>
+	);
+}
+
+render(<App />, document.getElementById('app'));
